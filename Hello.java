e35e9ee3b0a7df955fbd2b1f5fd36aa780e7647b
Kunal Anand <141386997+CodeWithKA@users.noreply.github.com>
2023-08-08 14:44:32 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,21 @@
+class Hello
+{
+    public static void main(String a[])
+    {
+        int num[] = new int[4];
+        num[0]= 12;
+        num[1]= 13;
+        num[2]= 14;
+        num[3]= 15;
+
+        for (int i=0;i<num.length;i++)
+        {
+            System.out.println(num[i]);
+        }
+
+
+    }
+}
+
+
+
